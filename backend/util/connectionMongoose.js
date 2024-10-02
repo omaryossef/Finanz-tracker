9e1dff032110f824a753a295427a8438dfebd0a8
omar <yossef>
2024-10-02 20:30:26 +0200

    update mongoose url

--- backend/util/connectionMongoose.js
+++ backend/util/connectionMongoose.js
@@ -2,13 +2,8 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 async function connectMongoose() {
-  const _pwd = "Oy76811148";
-  const _database = "products";
-  const _user = "omar";
-  const _cluster = "cluster0.taehtxj.mongodb.net";
-  const _uri = "mongodb+srv://omar:Jww5nz3lkde8sUOA@cluster0.9krnhbb.mongodb.net/Finanz-Tracker";
   try {
-    await mongoose.connect(_uri);
+    await mongoose.connect(process.env.DB_URL);
     const collections = (
       await mongoose.connection.db.listCollections().toArray()
     ).map((el) => el.name);
