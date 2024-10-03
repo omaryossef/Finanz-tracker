97f5329d001241443c2f965320cb6776d8efb0d5
omar <yossef>
2024-10-03 11:15:30 +0200

    new update

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,23 @@ import { connectMongoose } from "./util/connectionMongoose.js";
 
 const app = express();
 app.use(cors());
-// app.use((req, res, next) => {
-//   // res.setHeader(
-//   //   "Access-Control-Allow-Origin",
-//   //   "https://finanz-tracker.onrender.com"
-//   //   // "http://localhost:5173"
-//   // );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://finanz-tracker.onrender.com",
+    "http://localhost:5173"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 
 await connectMongoose();
+app.use("/test", (req, res) => res.send("Hello World"));
 app.use("/", userRoute);
 app.listen(3010, () => console.log("Server is on PORT:3010"));
